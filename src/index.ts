40f5b7baef2100f7511245e6161a05cfb3bf487b
Stroketon <igeeeorl22@gmail.com>
2020-10-11 15:17:43 +0300

    lol

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Application } from "express";
 import { join } from "path";
 import { config } from "dotenv"; config({ path: join(__dirname, "../.env") });
-const app: Application = express();
+const app = express();
 
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Welcome to the elerium.cc api" });
