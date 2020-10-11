53dac9786362f9734fb745705c0e4f8beb7eb9f1
Stroketon <igeeeorl22@gmail.com>
2020-10-11 11:24:31 +0300

    database and user has been added

---
+++ src/models/user.model.ts
@@ -0,0 +1,28 @@
+import { model, Schema, Document, SchemaTypes } from "mongoose"
+
+export interface IUser extends Document {
+  name: string;
+  key: string;
+  invites: number;
+  invitedBy: any;
+  redeemedKey: String;
+};
+
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  key: {
+    required: true,
+    type: String
+  },
+  invites: {
+    type: Number,
+    default: 0
+  },
+  invitedBy: SchemaTypes.ObjectId,
+  redeemedKey: String
+});
+
+export default model<IUser>("users", userSchema);
