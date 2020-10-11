893886cdc1aaee9a9cdddd499b1563532dfd1c7c
Stroketon <igeeeorl22@gmail.com>
2020-10-11 11:14:00 +0300

    ahhh too lazy to say what i did

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, Application } from "express";
+import { join } from "path";
+import { config } from "dotenv"; config({ path: join(__dirname, "../.env") });
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ success: true, message: "Welcome to the elerium.cc api" });
+});
+
+app.listen(process.env.PORT);
