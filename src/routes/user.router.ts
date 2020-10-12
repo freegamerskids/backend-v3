849e7f366bee56681b4af6e0214d61dc3c66c7e9
Stroketon <igeeeorl22@gmail.com>
2020-10-12 15:31:12 +0300

    better way on getting the user from the request

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
-import { User } from "../models/user.model";
 const router: Router = Router();
 
-router.get("/@me", authMiddleware, (req: any, res) => {
-  res.json(req.user);
+router.get("/@me", authMiddleware, (req, res) => {
+  res.json((req as any).user);
 });
 
 export default router;
