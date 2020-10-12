26fb873bb57d97c714bc5a5e6bb41c9402e15e1d
Stroketon <igeeeorl22@gmail.com>
2020-10-12 15:25:48 +0300

    aaa

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
+import { User } from "../models/user.model";
 const router: Router = Router();
 
 router.get("/@me", authMiddleware, (req: any, res) => {
-  res.json(req.user)
+  res.json(req.user);
 });
 
 export default router;
